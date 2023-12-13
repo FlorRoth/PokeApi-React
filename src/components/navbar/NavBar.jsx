9743350798e68f4d08d4c732a82cc53e15b08779
FlorRoth <roth.florencia99@gmail.com>
2023-12-13 15:42:18 -0300

    update navBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import {IonHeader,IonToolbar,IonTitle} from '@ionic/react';
+import {IonHeader,IonToolbar,IonTitle,IonImg, IonCol,IonRow} from '@ionic/react';
 
 export const NavBar = () => {
   return (
     <IonHeader translucent={false}>
-    <IonToolbar>
-      <IonTitle>Pokemones</IonTitle>
-    </IonToolbar>
+    <IonRow className="justify-content-center">
+      <IonCol size='auto'>
+        <IonImg
+        src={'src/images/logo-pokemon.webp'}
+        alt={`Logo Pokemon`}
+        style={{width: '180px'}}
+      />
+      </IonCol>
+    </IonRow>
   </IonHeader>
   )
 }
