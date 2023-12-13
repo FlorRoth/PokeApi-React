71add8edf2539dc8609ca7fe6fe37c4bb0dc3d2e
FlorRoth <roth.florencia99@gmail.com>
2023-12-13 14:34:58 -0300

    implementation usePaginate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';;
 import './styles/index.css'
-
+import { NavBar } from "./components/navbar/NavBar";
+import { usePaginate } from './hooks/usePaginate';
+import { PokemonQuery } from './components/PokemonQuery';
 
 
 function App() {
 
-
+  const limit = 16;
+  const {offset,page,onNext,onPrev} = usePaginate(0,limit,1);
+  
   return (
     <>
-      
-
+      <NavBar />
+      <PokemonQuery limit={limit} offset={offset} page={page} onNext={onNext} onPrev={onPrev}/>
     </>
   )
 }
